55009433c2c83fc00aa5133b95b7554dedba2dab
hazit77 <andriyhazit@gmail.com>
2017-05-05 11:39:57 +0300

    Update homework.java

--- src/homework.java
+++ src/homework.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/*
+виконав Житомірський Андрій
+
+Консольное приложение:
+- ввод размера массива int
+- ввод элементов массива
+- меню с возможными операциями над ним:
+- поиск чила по значению
+- сортировка
+
+*/
+
 
 public class homework {
     public static void main(String[] args) {
         //System.out.println("");
-        Scanner input = new Scanner(System.in); // Обяввляю Scanner
+        Scanner input = new Scanner(System.in);
         System.out.println("Введіть розмір мвсива: ");
-        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-        int array[] = new int[size]; // Создаём массив int размером в size
+        int size = input.nextInt();
+        int array[] = new int[size];
         System.out.println("Введіть елементи масива: ");
-    /*Пройдёмся по всему массиву, заполняя его*/
         for (int i = 0; i < size; i++) {
-            array[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+            array[i] = input.nextInt();
         }
         Scanner input1 = new Scanner(System.in);
         System.out.println ("Щоб відсортовати масив введіть 0 або 1 щоб не сортувати");
@@ -19,14 +30,24 @@ public class homework {
         for (int i = 0; i < size; i++) {
             if ( tf <=0) {
                 Arrays.sort(array);
-                System.out.println(" " + array[i]);  // Выводим на экран відсортований массив
+                System.out.println(" " + array[i]);
             }
             else{
                 System.out.println(" " + array[i]);
 
             }
+        }
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Ввдіть порядковий № елемента масива, щоб вивезти його на екран або введіть відємне число, для завершення програми ");
+        int pn = input.nextInt();
+        for (int i = 0; i < size; i++){
+            if (pn >= 0 ){                      // не знаю як виводити елемент масиву за номером, скажіт що треба почитати
+            System.out.println(" " + array[i]);
 
-
+            }
+            else {
+                System.out.println("Кінець!");
+            }
         }
         System.out.println();
     }
